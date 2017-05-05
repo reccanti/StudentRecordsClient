b15f19b108a481c5693e931bbf951d8bec51af69
Ben Wilcox <reccanti@gmail.com>
2017-05-05 04:48:31 -0400

    Create simple yargs cli
    Creates a simple yargs cli for listing course and majors

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import '../.env';
+import * as yargs from 'yargs';
+import axios from 'axios'
+
+const base_url = process.env["API_URL"];
+
+// fetch(`http://127.0.0.1/api/major`).then(function(res) {
+//     return res.text();
+// }).then(function (json) {
+//     console.log(json);
+// }).catch(function (err) {
+//     console.log(err);
+// });
+
+// axios.get(`http://127.0.0.1:8080/api/major`).then( res => {
+//     return console.log(res); 
+// });
+
+const argv = yargs
+    .usage('$0 <cmd> [args]')
+    .command('majors', 'Get a list of all majors')
+    .command('students', 'Get a list of students')
+    .command('courses', 'Get ta list of courses')
+    .help()
+    .argv;
+
+// display all majors
+if (argv._[0] === 'majors') {
+    console.log('\nMajors: \n')
+    axios
+        .get(`${base_url}/api/major`)
+        .then( response => {
+            response.data.map( major => {
+                console.log(`ID: ${major.id}`);
+                console.log(`Name: ${major.name}\n`);
+            });
+        });
+}
+
+// display all students
+else if (argv._[0] === 'courses') {
+    console.log('\nCourses: \n')
+    axios
+        .get(`${base_url}/api/course`)
+        .then( response => {
+            response.data.map( course => {
+                console.log(`ID: ${course.id}`);
+                console.log(`Name: ${course.name}`);
+                console.log(`Required Major ID: ${course.major_id}\n`);
+            });
+        });
+}
+
+// display all courses
